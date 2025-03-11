7d9ae577f55f770ef403dc0ff313068b3311649e
slabwu <134193743+slabwu@users.noreply.github.com>
2025-03-11 19:38:44 +0800

    Implement mergeSort()

---
+++ script.js
@@ -0,0 +1,29 @@
+
+console.log(mergeSort([1, 5, 6, 9, 8, 4]))
+
+function mergeSort(array) {
+    if (array.length === 1) return array;
+    let left = array;
+    let right = left.splice(Math.ceil(array.length / 2), Math.floor(array.length / 2));
+    let length = left.length + right.length;    
+
+    left = mergeSort(left);
+    right = mergeSort(right);
+
+    let output = [];
+    for (let i = 0; i < length - 1; i++) {
+        if (left[0] < right[0]) {
+            output.push(left.shift());
+            if (left.length === 0) {
+                output = output.concat(right);
+                return output;
+            }
+        } else {
+            output.push(right.shift());
+            if (right.length === 0) {
+                output = output.concat(left);
+                return output;
+            }
+        }
+    }
+}
