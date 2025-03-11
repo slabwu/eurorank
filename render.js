2822e7ef34376f5b8fc156852d773df213586735
slabwu <134193743+slabwu@users.noreply.github.com>
2025-03-11 23:12:34 +0800

    Add country and artist information

--- render.js
+++ render.js
@@ -1,17 +1,32 @@
 let left = document.querySelector(".left .details");
 let right = document.querySelector(".right .details");
 let result = document.querySelector(".result");
+const countryName = new Intl.DisplayNames(["en"], { type: "region" });
+
 
 export function renderOption(song, option) {
+    let ref;
     if (option === 1) {
-        left.innerHTML = `${song.song}`;
+        ref = left;
     } else {
-        right.innerHTML = `${song.song}`;
+        ref = right;
     }
+    ref.innerHTML = '';
+    ref.innerHTML += `<h1>${song.song}</h1>`;
+    ref.innerHTML += `<h2>${song.artist}</h2>`;
+    ref.innerHTML += `<h2>${countryName.of(`${song.country}`)} ${getFlagEmoji(`${song.country}`)}</h2>`;
 }
 
 export function renderResult(ranking) {
-    ranking.forEach((rank) => {
-        result.innerHTML += `<li>${rank.song}</li>`;
+    ranking.forEach((song) => {
+        result.innerHTML += `<li>${song.song} - ${song.artist} (${countryName.of(`${song.country}`)} ${getFlagEmoji(`${song.country}`)})</li>`;
     })
 }
+
+function getFlagEmoji(countryCode) {
+    const codePoints = countryCode
+      .toUpperCase()
+      .split('')
+      .map(char =>  127397 + char.charCodeAt());
+    return String.fromCodePoint(...codePoints);
+  }
